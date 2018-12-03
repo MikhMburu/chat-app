f6bb7840c11e7e2617572a355e41e2d5a07279f0
Michael N <mikhmburu@live.com>
2018-12-03 21:20:25 +0300

    socket.io modifications

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
 const port = process.env.PORT || 3000;
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname,"../public");
 // console.log(publicPath);
-const app = express();
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 //..define our express static folder
 app.use(express.static(publicPath));
 
+io.on("connection",(socket)=>{
+    console.log("New user connected. . .");
 
-app.listen(port,()=>{
+    socket.on("disconnect", () => {
+        console.log("Client disconnected. . ");
+    })
+});
+
+server.listen(port,()=>{
     console.log(`Chat app is up and running on port ${port}`);
 })
